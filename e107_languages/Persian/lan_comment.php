5830679ac50b00a2ea50d479e930995763c702ce
Mahdi Montazeri <i@mahdimontazeri.com>
2021-02-14 11:51:37 +0330

    Add files via upload

---
+++ e107_languages/Persian/lan_comment.php
@@ -0,0 +1,73 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Language File.
+|
+|     $Source: /cvs_backup/e107_0.8/e107_languages/English/lan_comment.php,v $
+|     $Revision$
+|     $Date$
+|     $Author$
++----------------------------------------------------------------------------+
+*/
+define("COMLAN_0", "[مسدود شده توسط مدیر]");
+define("COMLAN_1", "رفع انسداد");
+define("COMLAN_2", "مسدود کردن");
+define("COMLAN_4", "اطلاعات");
+define("COMLAN_5", "نظرات ...");
+define("COMLAN_6", "برای اظهار نظر در مورد این سایت باید وارد شوید - لطفا وارد شوید، یا اگر ثبت نام نکرده اید کلیک کنید");
+define("COMLAN_7", "مدیر اصلی سایت");
+define("COMLAN_8", "اظهار نظر");
+define("COMLAN_9", "ارسال نظر");
+define("COMLAN_10", "مدیر");
+define("COMLAN_11", "نتوانستید نظر خود را به پایگاه داده وارد کنید - لطفاً با کنار گذاشتن نویسه های غیر استاندارد دوباره تایپ کنید.");
+define("COMLAN_12", "کاربر");
+define("COMLAN_16", "نام کاربری:");
+define("COMLAN_100", "اخبار");
+define("COMLAN_101", "نظرسنجی");
+define("COMLAN_102", "پاسخ به:");
+define("COMLAN_103", "مقاله");
+define("COMLAN_104", "مرور");
+define("COMLAN_105", "محتوا");
+define("COMLAN_106", "بارگیری");
+define("COMLAN_145", "ثبت شده:");
+define("COMLAN_194", "مهمان");
+define("COMLAN_195", "عضو ثبت شده");
+define("COMLAN_310", "پذیرش پست به دلیل ثبت نام کاربری امکان پذیر نیست - اگر نام کاربری شماست لطفا برای ارسال به سیستم وارد شوید.");
+define("COMLAN_312", "پست تکراری - پذیرفتن امکان پذیر نیست.");
+define("COMLAN_313", "موقعیت");
+define("COMLAN_314", "اداره نظرات");
+define("COMLAN_318", "ویرایش نظر");
+define("COMLAN_319", "ویرایش شده");
+define("COMLAN_320", "بروز رسانی نظر");
+define("COMLAN_321", "اینجا");
+define("COMLAN_322", "برای ثبت نام");
+define("COMLAN_323", "خطا!");
+define("COMLAN_324", "موضوع");
+define("COMLAN_325", "دوباره:");
+define("COMLAN_326", "پاسخ به این");
+define("COMLAN_328", "نظرات قفل شده است");
+define("COMLAN_329", "غیرمجاز");
+define("COMLAN_330", "آی پی:");
+define("COMLAN_331", "منتظر تایید");
+define("COMLAN_332", "نظر حذف نشد");
+define("COMLAN_333", "نظر تایید شد");
+define("COMLAN_334", "نظر تایید نشد");
+define("COMLAN_335", "تایید شده");
+define("COMLAN_336", "لطفاً ابتدا چیزی بنویسید.");
+define("COMLAN_337", "با موفقیت به روز شد.");
+define("COMLAN_400", "تایید شده");
+define("COMLAN_401", "مسدود شده");
+define("COMLAN_402", "در انتظار");
+define("COMLAN_403", "پیام بگذارید...");
+define("COMLAN_404", "تایید");
+define("COMLAN_TYPE_1", "اخبار");
+define("COMLAN_TYPE_2", "بارگیری");
+define("COMLAN_TYPE_3", "پرسش و پاسخ");
+define("COMLAN_TYPE_4", "نظرسنجی");
+define("COMLAN_TYPE_5", "اسناد");
+define("COMLAN_TYPE_6", "مسیر اشکال");
+define("COMLAN_TYPE_7", "ایده ها");
+define("COMLAN_TYPE_8", "نمایه کاربر");
+define("COMLAN_TYPE_PAGE", "محتوا");
+define("COMLAN_500", "لطفاً برای ارسال نظر [وارد شوید].");
+define("COMLAN_501", "اگر هنوز ثبت نام نکرده اید ، می توانید [برای ثبت نام اینجا را کلیک کنید].");
